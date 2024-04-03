b0ab63db6771489055267e33c3eebc5ccf772d6b
Rhett Roseman <rroseman@rcplanners.com>
2024-04-03 12:14:53 -0400

    react-pdf./ renderer used to render pdf from png files

---
+++ pdf/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Document, Page, PDFViewer, Text, Image } from '@react-pdf/renderer';
+
+// Create a component for the PDF document
+const MyDocument = () => (
+  <Document style={{backgroundColor: 'green',}} >
+    <Page size="A4">
+      <Text style={{textAlign: 'center', margin: '10px'}}></Text>
+      <Image src='/assets/resume1.png'/>
+      <Image src='/assets/resume2.png'/>
+    </Page>
+  </Document>
+);
+
+// Use the PDFViewer component to display the PDF document in your React component
+const App = () => (
+  <div style={{  height: '100vh', width: '100vw',   display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'black'}}>
+    <PDFViewer width="70%" height="70%">
+      <MyDocument />
+    </PDFViewer>
+  </div>
+);
+
+export default App;
